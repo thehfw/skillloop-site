be90543cc06fa1e70217fc4148e382043db574d4
thehfw <harrisonwebb@mac.com>
2026-07-21 22:34:02 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,5 @@
 window.SKILLLOOP_CONFIG = {
-  SUPABASE_URL: 'https://vrnkegfqwaskgzceohbq.supabase.co/rest/v1/
-
-',
-  SUPABASE_ANON_KEY: 'sb_publishable_ioc5pumt7N1L5fnmu8pH8w_z11AyqpH
-',
+  SUPABASE_URL: 'https://vrnkegfqwaskgzceohbq.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_ioc5pumt7N1L5fnmu8pH8w_z11AyqpH',
   STRIPE_PUBLISHABLE_KEY: 'pk_test_YOUR_STRIPE_PUBLISHABLE_KEY',
 };
